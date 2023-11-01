6b2095cbe03d8f4b3a196ba4ee90d31bf4062c1d
enbiya onar <kartal.enbiya@hotmail.com>
2023-11-01 15:52:11 +0300

    Gorev9Tamamlandi

---
+++ Solution1/App.Web.Mvc/Data/Entity/CategoryPost.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App.Web.Mvc.Data.Entity
+{
+    public class CategoryPost
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PostId { get; set; }
+        [Required(ErrorMessage = "{0} boş geçilemez.")]
+        [DisplayName("Post Adı")]
+        [MinLength(10, ErrorMessage = "{0} en az {1} karakter olanilir!")]
+        public Post Post { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Solution1/App.Web.Mvc/Data/Entity/Post.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App.Web.Mvc.Data.Entity
+{
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} boş geçilemez.")]
+        [DisplayName("Post Adı")]
+        [StringLength(50, ErrorMessage = "{0} {1} karakterden fazla olamaz!")]
+        [MinLength(10, ErrorMessage = "{0} en az {1} karakter olanilir!")]
+        public string PostTitle { get; set; }
+
+        [Required(ErrorMessage = "{0} boş geçilemez.")]
+        [DisplayName("Post Adı")]
+        [StringLength(1000, ErrorMessage ="{0} {1} karakterden fazla olamaz!")]
+        [MinLength(10, ErrorMessage = "{0} en az {1} karakter olanilir!")]
+        public string PostContext { get; set; }
+        public List<CategoryPost> CategoryPosts { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
